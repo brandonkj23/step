fc6fbd3c9ef8cfbc8b0ad202d43c7ce921a2c2f9
brandonkj23 <brandonkj20012yahoo.com>
2020-06-11 12:40:25 -0400

    changed file name and it now returns the correct information

--- portfolio/src/main/java/com/google/sps/servlets/Loggedin.java
+++ portfolio/src/main/java/com/google/sps/servlets/Loggedin.java
@@ -22,24 +22,18 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.*;
 
 @WebServlet("/loggedin")
-public class loggedin extends HttpServlet {
+public class Loggedin extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html");
 
-    Query query = new Query("loginInfo");
-
-    DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-    PreparedQuery results = datastore.prepare(query);
-   
-    //Entity entity = results.asSingleEntity();
-    UserInfo userInfo = new UserInfo();
-    for (Entity entity : results.asIterable()) {
-      boolean isUserLoggedIn = (boolean) entity.getProperty("isUserLoggedIn");
-      String email = (String) entity.getProperty("email");
-      userInfo = new UserInfo(isUserLoggedIn,email);
-    }
+    UserService userService = UserServiceFactory.getUserService();
+    boolean isUserLoggedIn = userService.isUserLoggedIn();
+    String email = "";
+      if(isUserLoggedIn){
+       email = userService.getCurrentUser().getEmail();
+      }
+    UserInfo userInfo = new UserInfo(isUserLoggedIn,email);
       
 
     Gson gson = new Gson();
@@ -48,18 +42,5 @@ public class loggedin extends HttpServlet {
     response.getWriter().println(gson.toJson(userInfo));
   }
 
-  @Override
-    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
 
-        UserService userService = UserServiceFactory.getUserService();
-
-        Entity userEntity = new Entity("loginInfo");
-        userEntity.setProperty("isUserLoggedIn",userService.isUserLoggedIn());
-        userEntity.setProperty("email",userService.getCurrentUser().getEmail());
-
-        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
-        datastore.put(userEntity);
-
-        response.sendRedirect("/index.html");
-    }
 }
