2ee362c6ab03f3cc9d3a5d955f7d42dff9a397d6
brandonkj23 <brandonkj20012yahoo.com>
2020-06-11 14:14:45 -0400

    add loginurl attribute

--- portfolio/src/main/java/com/google/sps/data/UserInfo.java
+++ portfolio/src/main/java/com/google/sps/data/UserInfo.java
@@ -5,9 +5,11 @@ public class UserInfo{
 
     private boolean isUserLoggedIn;
     private String email;
+    private String logInUrl;
 
-    public UserInfo(boolean isUserLoggedIn, String email){
+    public UserInfo(boolean isUserLoggedIn, String email, String logInUrl){
         this.isUserLoggedIn = isUserLoggedIn;
         this.email = email;
+        this.logInUrl = logInUrl;
     }
 }
