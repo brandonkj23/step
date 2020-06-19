41eeee2ef78f44cad7fdf30404fb7c716b87ce95
brandonkj23 <brandonkj20012yahoo.com>
2020-06-19 10:38:31 -0400

    changed how the named is trimmed

--- walkthroughs/week-5-tdd/intro/src/main/java/com/google/sps/Greeter.java
+++ walkthroughs/week-5-tdd/intro/src/main/java/com/google/sps/Greeter.java
@@ -22,7 +22,6 @@ public class Greeter {
    * Returns a greeting for the given name.
    */
   public String greet(String name) {
-    String newName = name.replaceAll("\\s", "");
-    return "Hello " + newName;
+    return "Hello " + name.trim();
   }
 }
